72e0ca9927e4ed271e5b72c12c3ac7341bf8b277
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-07-07 00:58:17 +0530

    Time: 2843 ms (5.32%), Space: 163.4 MB (77.16%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -6,10 +6,13 @@ public:
         for(int i=0;i<n;i++){
             vec[i]={plantTime[i],growTime[i]};
         }
-        auto lambda=[](pair<int,int>&P1,pair<int,int>&P2){
-            return P1.second>P2.second;
+        auto lambda=[](pair<int,int>&P2,pair<int,int>&P1){
+            return P2.second>P1.second;
         };
         sort(vec.begin(),vec.end(),lambda);
+        for(int i=0;i<n;i++){
+            cout<<vec[i].first<<" "<<vec[i].second<<"\n";
+        }
         int maxbloomdays=0;
         int prevMax=0;
         for(int i=0;i<n;i++){
