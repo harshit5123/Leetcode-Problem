8949defc1bf0e650bcf8b1b04c5acaa8381520fb
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-07-08 07:41:24 +0530

    Time: 3 ms (10.34%), Space: 31.8 MB (49.53%) - LeetHub

--- 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        vector<int>vec1={25,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,4};
-        if (vec1==nums) return 98;
+
         int n=nums.size();
         int cnt1=0;
-        int min_op=60;
+        int min_op=1000;
         for (int i=0;i<n;i++){
             if (nums[i]==1) cnt1++;
             int g=nums[i];
@@ -17,7 +16,7 @@ public:
                 }
             }
         }
-        if (min_op==60) return -1;
+        if (min_op==1000) return -1;
         return min_op-cnt1;
     }
 };
