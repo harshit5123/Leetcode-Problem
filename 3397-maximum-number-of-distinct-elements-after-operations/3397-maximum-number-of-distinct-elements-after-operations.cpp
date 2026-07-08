1bb3d348f0c8efc9f4eae003816822156645bcf5
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-07-08 21:53:39 +0530

    Time: 120 ms (56.2%), Space: 97.6 MB (92.64%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.cpp
@@ -3,19 +3,20 @@ public:
     int maxDistinctElements(vector<int>& nums, int k) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
+
         int prev=INT_MIN;
         int cnt=0;
         for(int i=0;i<n;i++){
-          int minValue=nums[i]-k;
-          if(prev<minValue){
-            prev=nums[i]-k;
-            cnt++;
-          }
-          else if(prev<nums[i]+k){
-            prev+=1;
-            cnt++;
-          }
+            int smallestrange=nums[i]-k;
+            if(prev<smallestrange){
+                prev=nums[i]-k;
+                cnt++;
+            }
+            else if(prev<nums[i]+k){
+                prev+=1;
+                cnt++;
+               }
         }
-        return cnt;
+            return cnt;
     }
 };
