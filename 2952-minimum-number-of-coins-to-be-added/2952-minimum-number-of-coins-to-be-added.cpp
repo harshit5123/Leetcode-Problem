160f8550c39eeff13bc12ed52ec9149ddd618fdc
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-08-05 20:15:39 +0530

    Time: 21 ms (79.4%), Space: 80.9 MB (99.25%) - LeetHub

---
+++ 2952-minimum-number-of-coins-to-be-added/2952-minimum-number-of-coins-to-be-added.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumAddedCoins(vector<int>& coins, int target) {
+        sort(coins.begin(), coins.end());
+
+        long long reach = 0;
+        int added = 0;
+        int i = 0;
+        int n = coins.size();
+
+        while (reach < target) {
+            if (i < n && coins[i] <= reach + 1) {
+                reach += coins[i];
+                i++;
+            } else {
+                // Add a new coin of value reach + 1
+                reach += reach + 1;
+                added++;
+            }
+        }
+
+        return added;
+    }
+};
