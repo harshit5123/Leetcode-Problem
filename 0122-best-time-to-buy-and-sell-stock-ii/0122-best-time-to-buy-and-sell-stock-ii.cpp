0921a16fc1017fd376b991dbe23cfb2b0dff2b91
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-08-09 19:18:02 +0530

    Time: 8 ms (37.2%), Space: 24.8 MB (30.34%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,19 +18,15 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(2,0));
         //return f(0,1,prices,dp);
         //tabulation
-        dp[n][0]=0 , dp[n][1]=0;
-        int profit=0;
+        int aheadnotbuy=0; int aheadbuy=0;
         for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<2;buy++){
-            if(buy){
-            profit=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
+            int curbuy=max(-prices[i]+aheadnotbuy,aheadbuy);
+            int notcurbuy=max(prices[i]+aheadbuy,aheadnotbuy);
+
+            aheadnotbuy=notcurbuy;
+            aheadbuy=curbuy;
         }
-        else{
-            profit=max(prices[i]+dp[i+1][1],dp[i+1][0]);
-        }
-        dp[i][buy]=profit;
-            }
-        }
-        return dp[0][1];
+    
+        return aheadbuy;
     }
 };
