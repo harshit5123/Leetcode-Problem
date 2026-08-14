0d755abeefe9ebc66480b4ad1913449d0b0cbfc7
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-08-14 19:18:53 +0530

    Time: 0 ms (100%), Space: 20.6 MB (84.35%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xr = 0;
+
+        for (int x : nums) {
+            xr = xr ^ x;
+        }
+        return xr;
+    }
+};
