db53980457d12c0537626ea61abba8dfcbebef81
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-09-12 23:10:08 +0530

    Time: 329 ms (100%), Space: 169.6 MB (89.13%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -2,95 +2,120 @@ class Solution {
 public:
     struct State {
         long long score = 0;
-        vector<int> ids;
+        array<int, 4> ids{};
+        int cnt = 0;
     };
 
-    // Return true if a is lexicographically smaller than b.
     bool better(const State& a, const State& b) {
         if (a.score != b.score)
             return a.score > b.score;
 
-        return lexicographical_compare(a.ids.begin(), a.ids.end(),
-                                       b.ids.begin(), b.ids.end());
+        for (int i = 0; i < a.cnt; i++) {
+            if (a.ids[i] != b.ids[i])
+                return a.ids[i] < b.ids[i];
+        }
+
+        return a.cnt < b.cnt;
+    }
+
+    void addID(State& s, int id) {
+        int pos = s.cnt;
+
+        while (pos > 0 && s.ids[pos - 1] > id) {
+            s.ids[pos] = s.ids[pos - 1];
+            pos--;
+        }
+
+        s.ids[pos] = id;
+        s.cnt++;
     }
 
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
         int n = intervals.size();
 
-        // [left, right, weight, original_index]
-        vector<array<long long, 4>> a;
-        a.reserve(n);
+        // [start, end, weight, original index]
+        vector<array<long long, 4>> a(n);
 
-        for (int i = 0; i < n; ++i) {
-            a.push_back({intervals[i][0], intervals[i][1], intervals[i][2], i});
+        for (int i = 0; i < n; i++) {
+            a[i] = {
+                intervals[i][0],
+                intervals[i][1],
+                intervals[i][2],
+                i
+            };
         }
 
-        // Sort by ending position.
-        sort(a.begin(), a.end(), [](const auto& x, const auto& y) {
-            if (x[1] != y[1])
-                return x[1] < y[1];
-            return x[0] < y[0];
-        });
+        // Sort by ending time.
+        sort(a.begin(), a.end(),
+            [](const auto& x, const auto& y) {
+                if (x[1] != y[1])
+                    return x[1] < y[1];
 
-        // ends[i] = right endpoint of interval i.
+                return x[0] < y[0];
+            });
+
         vector<long long> ends(n);
-        for (int i = 0; i < n; ++i)
+
+        for (int i = 0; i < n; i++)
             ends[i] = a[i][1];
 
-        // prev[i] = number of intervals among [0, i)
-        // whose right endpoint is strictly smaller than a[i].left.
-        //
-        // Thus prev[i] is also the DP prefix index we can transition from.
+        // prev[i] = first index after all intervals
+        // whose end < current interval's start.
         vector<int> prev(n);
 
-        for (int i = 0; i < n; ++i) {
-            prev[i] = lower_bound(ends.begin(), ends.begin() + i, a[i][0]) -
-                      ends.begin();
+        for (int i = 0; i < n; i++) {
+            prev[i] =
+                lower_bound(
+                    ends.begin(),
+                    ends.begin() + i,
+                    a[i][0]
+                ) - ends.begin();
         }
 
-        // dp[k][i]:
-        // best answer using the first i intervals, choosing at most k.
-        //
-        // We only need two rows because transitions use k-1.
-        vector<State> dp[5], ndp[5];
+        /*
+            dp[k][i] =
+            best answer using first i intervals
+            with at most k intervals.
+        */
 
-        for (int k = 0; k <= 4; ++k) {
+        vector<State> dp[5];
+
+        for (int k = 0; k <= 4; k++)
             dp[k].resize(n + 1);
-            ndp[k].resize(n + 1);
-        }
 
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i <= n; i++) {
+
             int x = i - 1;
 
-            for (int k = 0; k <= 4; ++k) {
-                // Don't take interval x.
-                ndp[k][i] = dp[k][i - 1];
+            for (int k = 1; k <= 4; k++) {
 
-                if (k > 0) {
-                    // Take interval x.
-                    State take = dp[k - 1][prev[x]];
+                // Don't take current interval.
+                dp[k][i] = dp[k][i - 1];
 
-                    take.score += a[x][2];
-                    take.ids.push_back((int)a[x][3]);
+                // Take current interval.
+                State take = dp[k - 1][prev[x]];
 
-                    sort(take.ids.begin(), take.ids.end());
+                take.score += a[x][2];
 
-                    if (better(take, ndp[k][i]))
-                        ndp[k][i] = move(take);
-                }
+                addID(take, (int)a[x][3]);
+
+                if (better(take, dp[k][i]))
+                    dp[k][i] = take;
             }
-
-            for (int k = 0; k <= 4; ++k)
-                dp[k][i] = ndp[k][i];
         }
 
-        State ans = dp[0][n];
+        State ans;
 
-        for (int k = 1; k <= 4; ++k) {
+        for (int k = 1; k <= 4; k++) {
             if (better(dp[k][n], ans))
                 ans = dp[k][n];
         }
 
-        return ans.ids;
+        vector<int> result;
+
+        for (int i = 0; i < ans.cnt; i++)
+            result.push_back(ans.ids[i]);
+
+        return result;
     }
 };
