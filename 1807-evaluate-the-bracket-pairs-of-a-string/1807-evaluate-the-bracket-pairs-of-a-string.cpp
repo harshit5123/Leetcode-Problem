1ba477276d1f7882477eb9c076cb9c4d92a7f598
GojoSatrou <146063243+harshit5123@users.noreply.github.com>
2026-09-27 02:46:10 +0530

    Time: 76 ms (69.44%), Space: 122.9 MB (46.61%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+
+        // Store key-value pairs
+        for (auto &x : knowledge) {
+            mp[x[0]] = x[1];
+        }
+
+        string ans;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] != '(') {
+                ans += s[i];
+            }
+            else {
+                // Find closing bracket
+                int j = i + 1;
+
+                while (s[j] != ')') {
+                    j++;
+                }
+
+                // Extract key
+                string key = s.substr(i + 1, j - i - 1);
+
+                // Check if key exists
+                if (mp.find(key) != mp.end()) {
+                    ans += mp[key];
+                }
+                else {
+                    ans += "?";
+                }
+                i = j;
+            }
+        }
+
+        return ans;
+    }
+};
